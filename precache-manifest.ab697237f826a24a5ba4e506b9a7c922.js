21340ea31aeb2d198b33e27c3bc311955863b6a3
Vianny Reyes <reyesvi92@gmail.com>
2020-04-14 10:41:17 -0400

    Updates

---
+++ precache-manifest.ab697237f826a24a5ba4e506b9a7c922.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4beb11bab442d54c8d4ba33997800225",
+    "url": "/Job-Application/index.html"
+  },
+  {
+    "revision": "575dc71fae99ddc08bf6",
+    "url": "/Job-Application/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "60fe3f71c4d42cdc98c7",
+    "url": "/Job-Application/static/css/main.5e62c3db.chunk.css"
+  },
+  {
+    "revision": "575dc71fae99ddc08bf6",
+    "url": "/Job-Application/static/js/2.64d81074.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Job-Application/static/js/2.64d81074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60fe3f71c4d42cdc98c7",
+    "url": "/Job-Application/static/js/main.ba3b0030.chunk.js"
+  },
+  {
+    "revision": "b6c97d63200d3436b676",
+    "url": "/Job-Application/static/js/runtime-main.35395cd3.js"
+  },
+  {
+    "revision": "0afe1f74644078e30c5ee5d2f3ecd33d",
+    "url": "/Job-Application/static/media/burger.0afe1f74.png"
+  },
+  {
+    "revision": "b90dec75e1e43824ebb43bbc45561433",
+    "url": "/Job-Application/static/media/fondo.b90dec75.jpg"
+  },
+  {
+    "revision": "1843dd52fbfc2d858227536c4847d875",
+    "url": "/Job-Application/static/media/miFoto.1843dd52.jpeg"
+  },
+  {
+    "revision": "7f887b3a25fd5d5ddfe5626a1231aad6",
+    "url": "/Job-Application/static/media/peliculas.7f887b3a.png"
+  },
+  {
+    "revision": "e77cad0c104a4907b41eaa656c6b7538",
+    "url": "/Job-Application/static/media/red.e77cad0c.png"
+  }
+]);
